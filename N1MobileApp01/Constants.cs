2fc405ba29a8b3f280a85ed67ce98fa67b43c405
unknown <garagate.p@gmail.com>
2018-06-01 11:28:15 +0700

    test

---
+++ N1MobileApp01/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace N1MobileApp01
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://n1mobileapp01.azurewebsites.net";
+	}
+}
+
